c794a8a40340b9e4d435ef56a0d67cdb9fa61ed4
Alisher <alisheraxmedov0012@gmail.com>
2026-09-03 21:44:27 +0500

    Add dependencies to requirements.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "uz.qalqon.app"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -45,7 +45,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
 
     packaging {
