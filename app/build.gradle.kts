02b11813b713e94eca00984e71f4ab13faa0fb91
Alisher <alisheraxmedov0012@gmail.com>
2026-09-03 13:56:55 +0500

    Add dependencies to requirements.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation(composeBom)
     androidTestImplementation(composeBom)
